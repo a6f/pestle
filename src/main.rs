6caeeab99594f8abaf7c0401c4a0962679f17d80
Ambrose Feinstein <ambrose@osdyne.com>
2024-12-14 02:44:03 -0800

    convert to proc macro

--- src/main.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    let filename = std::env::args().nth(1).unwrap();
-    let code = pestle::generate_typed_rules(&filename);
-    let ast = syn::parse2(code).unwrap();
-    let formatted = prettyplease::unparse(&ast);
-    print!("{formatted}");
-}
